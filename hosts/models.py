4764b31f00f216bee9712c0f1dbe10faec2ad7b8
Marcos Paulo <marcos_ppm@live.com>
2020-03-08 01:52:00 -0300

    added blank=True to busy_dates textfield

--- hosts/models.py
+++ hosts/models.py
@@ -12,7 +12,7 @@ class Bedroom(models.Model):
     name = models.CharField("Nome", max_length=50)
     host = models.ForeignKey(Host, on_delete=models.CASCADE)
     maximum_guests = models.IntegerField("Número de hóspedes")
-    busy_dates = models.TextField("Datas reservadas")
+    busy_dates = models.TextField("Datas reservadas", blank=True)
     purchased = models.BooleanField("Vendido")
     def __str__(self):
         return self.name
